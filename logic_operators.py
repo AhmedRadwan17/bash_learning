85c56dd59cac6125f4d017e0ffdec262375bf411
Ahmed Radwan <marimradwan922@gmail.com>
2025-02-21 02:31:52 +0200

    Update logic_operators.py

--- logic_operators.py
+++ logic_operators.py
@@ -5,11 +5,12 @@ read n1
 echo "Enter Second Number:"
 read n2
 
-if [ "$n1" -lt 0 -a "$n2" -lt 0 ]; then
+if [ "$n1" -lt 0 -a "$n2" -lt 0 ]; then # and = -a
     echo "Both numbers are negative"
-elif [ "$n1" -lt 0 -o "$n2" -lt 0 ]; then
+elif [ "$n1" -lt 0 -o "$n2" -lt 0 ]; then # or = -o
     echo "At least one number is negative"
-elif [ ! "$n1" -eq 0 ]; then
+elif [ ! "$n1" -eq 0 ]; then  
+# not = !
     echo "First number is NOT zero"
 else
     echo "No conditions met"
